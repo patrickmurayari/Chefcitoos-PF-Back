99792d55464c2e3adac7408a8451d9920b81c5e9
patrickmurayari <patrickmurayari03@gmail.com>
2023-06-09 22:24:22 -0300

    port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const server = require('./src/server');
+const PORT = process.env.PORT || 3001
 
 
-
-server.listen(3001, () => {
+server.listen(PORT, () => {
     console.log("Listening in port", 3001)
 })
